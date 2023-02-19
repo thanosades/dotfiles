8955b31a00b5dc4b2d7355e6633a1782eaf3ef9b
Thanos Dimitriades <thanosades@gmail.com>
2023-02-19 19:22:23 +0200

    add lualine

--- .config/nvim/lua/thanosades/packer.lua
+++ .config/nvim/lua/thanosades/packer.lua
@@ -60,6 +60,11 @@ return require('packer').startup(function(use)
 	  }
   }
 
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
